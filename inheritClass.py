c6d88f8869143925a23ffbcc2f0fda0cd5c02eca
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2020-09-23 01:32:50 -0700

    Add files via upload

---
+++ inheritClass.py
@@ -0,0 +1,13 @@
+class User:
+    name = 'Isaac'
+    email = ' '
+    password = '13071996'
+    account_number = 503
+
+class Student(User):
+    major = 'Computer Science'
+    number_of_credits = 12
+
+class GPA(User):
+    GPA = 3.75
+    grading = 'A'
