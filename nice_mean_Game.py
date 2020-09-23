071b1027d2462dd2a501cda57b1e33463d5d2116
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2020-09-22 19:23:00 -0700

    Add files via upload
    fixed the error

--- nice_mean_Game.py
+++ nice_mean_Game.py
@@ -51,21 +51,21 @@ def show_score(nice,mean,name):
     print("\n{}, your current total: \n{}, Nice) and ({}, Mean)".format(name,nice,mean))
 
 
-def score(name,nice,mean):
+def score(nice,mean,name):
     #score function is being passed the values stored within the 3 variables
     if nice > 2:  #if condition is valid, call win function passing in the variables so it can use them
-        win(name,nice,mean)
+        win(nice,mean,name)
     if mean > 2:  #if condition is valid, call lose function passing in the variables so it can use them
-        lose(name,nice,mean)
+        lose(nice,mean,name)
     else:         #else, call nice_mean function passing in the variables so it can usde them
-        nice_mean(name,nice,mean)
+        nice_mean(nice,mean,name)
 
 
-def win(name,nice,mean):
+def win(nice,mean,name):
     #Substitute the {} wildcards with our variables values
     print("\nNice job {}, you win! \nEveryone loves you and you've \nmade lots of friends along the way!".format(name))
     #call again function and pass in our variables
-    again(name,nice,mean)
+    again(nice,mean,name)
 
 
 def lose(name,nice,mean):
@@ -75,7 +75,7 @@ def lose(name,nice,mean):
     again(nice,mean,name)
 
 
-def again(name,nice,mean):
+def again(nice,mean,name):
     stop = True
     while stop:
         choice = input("\nDo you want to play again? (y/n):\n>>> ").lower()
@@ -90,11 +90,11 @@ def again(name,nice,mean):
             print("\nEnter ( Y ) for 'YES', ( N ) for 'NO':\n>>> ")
 
 
-def reset(name,nice,mean):
+def reset(nice,mean,name):
     nice = 0
     mean = 0
     #Notice, I do not reset the name variable as that same user has elected to play again
-    start(name,nice,mean)
+    start(nice,mean,name)
 
 
 
