5c1087e145ad5cebd15ab2466f3ffd94eea04505
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2020-09-24 14:12:04 -0700

    Add files via upload

---
+++ fileNamedbs.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+conn = sqlite3.connect('file.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_file( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        File_Name TEXT \
+        )")               
+    conn.commit()
+conn.close()
+
+conn = sqlite3.connect('file.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute
+
+    fileList = ('information.docx','Hello.txt','myImage.png', \
+               'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
+
+    conn.commit()
+conn.close()
+
+conn = sqlite3.connect('test.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute
+
+    for i in fileList:
+        if i.endswith('.txt'):
+            ("INSERT INTO tbl_persons(File_name) VALUES (?)")
+            print(i)
+
+        
+                       
+                           
+        
+    
+
+
+        
+           
+        
+
+
+        
+
+
