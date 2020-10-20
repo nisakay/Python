530a33e8999022da0fb8e2cd578e515e523e1502
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2020-10-19 21:35:24 -0700

    Django Check book

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Classes.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_Checkbook.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
