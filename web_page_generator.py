7a51d860515b9d1de82835039d9cf625d07faacb
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2020-10-27 03:48:32 -0700

    Second Commit

--- web_page_generator.py
+++ web_page_generator.py
@@ -1,22 +1,27 @@
 from tkinter import *
 import web_page_gui
+import webbrowser
 
-def addToBody(self):
-    f = open("webPage_generator.html", "a")
-    f.write("""<html> 
+
+
+def userInput(self):
+    userInput =  self.txt_body.get()
+
+    # Writing to file 
+    with open("webPage_generator.html", "a") as file1:
+        file1.write("""<html> 
                  <body> 
                     <h1>
                           Stay tuned for our amazing summer sale! 
                     </h1>
                  </body> 
             </html> """)
-    f.close()
+        file1.writelines(userInput)
+    # Reading from file
+    with open("webPage_generator.html", "r+") as file1:
+        print(file1.read())
 
-    #open and read the file after the appending:
-    f = open("webPage_generator.html", "r")
-    print(f.read())
-
-
+    webbrowser.open_new_tab("webPage_generator.html")
 
 
 if __name__ == "__main__":
--- web_page_gui.py
+++ web_page_gui.py
@@ -11,15 +11,12 @@ class ParentWindow(Frame):
         self.master.minsize(500,300) #(Height, Width)
         self.master.maxsize(500,300)
         
-        self.btn_add = tk.Button(self.master,width=30,height=2,text='Set a new body text of your choice',command=lambda: web_page_generator.addToBody(self))
+        self.btn_add = tk.Button(self.master,width=30,height=2,text='Set a new body text of your choice',command=lambda: web_page_generator.userInput(self))
         self.btn_add.grid(row=0,column=2,padx=(100,80),pady=(10,0),sticky=W)
 
-        self.txt_body = tk.Entry(self.master,text='')
+        self.txt_body = tk.Entry(self.master,text='',textvariable=lambda: web_page_generator.userInput(self))
         self.txt_body.grid(row=1,column=2,rowspan=1,columnspan=2,padx=(100,80),pady=(30,40),sticky=N+E+W)
-        
 
-       
-        
 
                    
 
